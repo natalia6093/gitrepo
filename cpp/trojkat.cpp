1633bef92ef9a4a647d7d26a436a2b49c2043691
natalia6093 <nzacharska142@gmail.com>
2017-09-26 10:36:59 +0200

    pole i obwód

---
+++ cpp/trojkat.cpp
@@ -0,0 +1,24 @@
+/*
+ * hello.cpp
+ */
+ 
+
+
+#include  <iostream>
+
+using namespace std;
+int main(int argc, char **argv)
+{
+
+    
+    int  a, b, c; 
+    a = b = c = 0; 
+    
+	cout << "Podaj boki: " << endl;
+	cin >> a >> b >> c;
+    cout << "Obwód: " << a + b + c << endl;
+        
+    
+	return 0;
+}
+

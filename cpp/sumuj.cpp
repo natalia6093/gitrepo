7a794153f23d48920813f97f821a0d7b0604e930
natalia6093 <nzacharska142@gmail.com>
2017-10-03 09:53:37 +0200

    sumuj

--- cpp/sumuj.cpp
+++ cpp/sumuj.cpp
@@ -8,13 +8,12 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	int i;
-    int j;
+	int suma = 0;
     
-    for (i = 1; i < 101; i++)
-    {
-        
-    }
+    for (int i = 1; i < 11; i++) suma = suma +i;
+    
+        cout << suma;
+    
 	return 0;
 }
 

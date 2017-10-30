2d48fb490916fe5c60adecc4a1499123ae66ef6a
natalia6093 <nzacharska142@gmail.com>
2017-10-30 12:35:25 +0100

    python

---
+++ python/wiataj.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#osoba = "Adam Słodowy"
+#osoba = 'Adam Słodowy'
+osoba = input('Jak się nazywasz?')
+wiek = input ('ile masz lat?')
+print('Witaj,' , osoba, '!')
+print('Urodziłeś się w ', 2017 - int(wiek))
+rok_pythona = 1991 
+wiek_pythona = 2017 - rok_pythona
+
+if wiek_pythona > int (wiek):
+    print ('Jestem starszy!')
+elif wiek_pythona < int (wiek):
+    print('Jestem młodszy!')
+else:
+    print ('Mamy tyle samo lat!')
+

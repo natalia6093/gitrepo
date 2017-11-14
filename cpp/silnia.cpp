ca72511209e611ff6a78a6a62a328c676d497df7
natalia6093 <nzacharska142@gmail.com>
2017-11-14 10:35:50 +0100

    silnia

---
+++ cpp/silnia.cpp
@@ -0,0 +1,24 @@
+/*
+ * silnia.cpp
+ 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int silnia_it(n);
+
+int main(int argc, char **argv)
+{
+	int wynik = 1
+    for (int i = 1; i <= int n ; i++){
+        wynik = wynik * 1
+        }
+    return wynik;
+    
+    
+	return 0;
+}
+
---
+++ python/silnia.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  silnia.py
+#  n! = 1 dla n={0, 1}
+#  n! = 1 * ... * n dla N+ - {1}
+
+def silnia_it(n):
+    wynik = 1
+    for i in  range(2, n + 1):
+        wynik = wynik * i
+    return wynik
+    
+
+def main(args):
+    """ Funkcja główna"""
+    # pobierz od uzytkownika liczbę naturalną 
+    # i przypisz ja do zmiennej n
+    # wywołajfunkcje silnia it() z odpowiedznim argumentem
+    n = int(input('Podaj liczbę naturalną'))
+    print(silnia_it(n))
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))

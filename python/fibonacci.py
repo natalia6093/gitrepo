e36f9382ad87665ccaf1341b0a3a580d2c30eb8e
natalia6093 <nzacharska142@gmail.com>
2017-11-28 10:36:51 +0100

    fibonacci3

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -8,19 +8,29 @@ def fib_iter(n):
     elif n == 1:
         return 1
     a, b = (0, 1)
-    for i in range(1, n - 1):
+    print(a)
+    for i in range(1, n):
         a, b = b, a + b
-        print(a, " ", b)
+        print(a, " ", b, " ", b / a)
     return b
 
 
+def fib_iter2(n):
+    a, b = (1, 1)
+    print(a)
+    print(b)
+    for i in range(2, n):
+        a, b = b, a + b
+        print()
+
+
 def main(args):
-    n = int(input('Podaj wyraz ciągu: '))
+    #  n = int(input('Podaj wyraz ciągu: '))
     #  assert fib_iter(0) == 0
     #  assert fib_iter(1) == 1
     #  assert fib_iter(2) == 1
     #  assert fib_iter(5) == 5
-    print("Wyraz {:d} = {:d}".format(n, fib_iter(n)))
+    print("Wyraz {:d} = {:d}".format(20, fib_iter(20)))
     return 0
 
 

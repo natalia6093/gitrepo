f93758b63523f577f54636739774a961de667451
natalia6093 <nzacharska142@gmail.com>
2017-11-28 10:16:46 +0100

    żyła

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 def fib_iter(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
     a, b = (0, 1)
     for i in range(1, n - 1):
-        a = b
-        b = a + b
-    if n > 0:
-        return b
-    else:
-        return a
+        a, b = b, a + b
+        print(a, " ", b)
+    return b
+
 
 def main(args):
     n = int(input('Podaj wyraz ciągu: '))
-    assert fib_iter(0) == 0
-    assert fib_iter(1) == 1
-    assert fib_iter(2) == 1
-    assert fib_iter(5) == 5
+    #  assert fib_iter(0) == 0
+    #  assert fib_iter(1) == 1
+    #  assert fib_iter(2) == 1
+    #  assert fib_iter(5) == 5
     print("Wyraz {:d} = {:d}".format(n, fib_iter(n)))
     return 0
 

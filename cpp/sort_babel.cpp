3f3635c58857950d173838875fec9c4f4de16d40
natalia6093 <nzacharska142@gmail.com>
2017-12-19 10:33:05 +0100

    sort4

---
+++ cpp/sort_babel.cpp
@@ -0,0 +1,51 @@
+/*
+ * sort_wybor.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std; 
+
+void wypelnij(int t[], int n, int maks){
+    srand(time(NULL)); // inicjacja generatora liczb pseudolosowych
+    for (int i = 0; i < n; i++){
+        t[i] = 1 + rand() % maks; // losowanie liczb całkowitych <0,maks>
+        
+    }
+    
+}
+
+void drukuj(int t[], int n){
+        for (int i = 0; i < n; i++){
+        cout << t[i] << " ";
+    }
+    cout << endl; 
+    
+}
+
+void zamien(int &a, int &b){
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
+void sort_wyb(int t[], int n){
+    cout << "------------Sortowanie bąbelkowe ----------------"<< endl;
+    for(int i = 0; i < n; i++ ){
+        for (int j = 0; j < n - 1 - i)
+    }
+}
+
+int main(int argc, char **argv)
+{
+	const int ile = 10;
+    int tab[ile];
+    wypelnij(tab,ile,20);
+    drukuj(tab,ile);
+    sort_bable(tab, ile);
+    drukuj(tab,ile);
+	return 0;
+}
+
+

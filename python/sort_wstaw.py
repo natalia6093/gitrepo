808e5ec57a4a7c5143e88fce086adbdda6e6614b
natalia6093 <nzacharska142@gmail.com>
2018-01-02 10:34:55 +0100

    sort wstaw

---
+++ python/sort_wstaw.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  sort_wstaw.py
+
+def sort_wstaw(lista):
+    """wersja liniowa"""
+    for i in range (1, len(lista)):
+        el = lista[i]
+        k = i - 1
+        while k >= 0 and lista[k] > el: # wyszukiwanie pozycji
+            lista[k + 1] = lista[k] # przesuwanie elementów
+            k -= 1
+        lista[k + 1] = el 
+    return lista
+def main(args):
+    lista = [4, 3, 7, 0, 2, 3, 1, 9]
+    print(lista)
+    # [3, 4, 7, 0, 2, 3, 1, 9]
+    # [3, 4, 7, 0, 2, 3, 1, 9]
+    # [0, 3, 4, 7, 2, 3, 1, 9]
+    print(sort_wstaw(lista))
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))

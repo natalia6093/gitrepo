1bff43de81998dae25db577b035932bf8a2ebee4
natalia6093 <nzacharska142@gmail.com>
2018-01-05 08:05:23 +0100

    sort wstaw

---
+++ cpp/sort_wstaw.cpp
@@ -0,0 +1,60 @@
+/*
+ * sort_wstaw.cpp
+ */
+
+#include <iostream>
+
+using namespace std;
+
+void sort_wstaw(int t[], int n)
+{
+    int el=0;
+    int k=0;
+    
+    for(int i=1;i<n;i++)
+    {
+        el = t[i];
+        k = i-1;
+        
+        while(k>=0 && t[k] > el)
+        {
+            t[k+1] = t[k];
+            k--;
+        }
+        t[k+1] = el;
+    }
+}
+void drukuj(int tab[], int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        cout<<tab[i]<<" ";
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int ile = 10;
+    int lista[ile];
+    
+    lista[0]= 2; 
+    lista[1]= 5; 
+    lista[2]= 8; 
+    lista[3]= 9; 
+    lista[4]= 0; 
+    lista[5]= 3; 
+    lista[6]= 6; 
+    
+    //int lista [5] =  {3, 4, 5, 6, 7}
+    
+    cout<<"Przed sortowaniem: "<<endl;
+    drukuj(lista, ile);
+    
+    cout<<endl;
+    cout<<"Po sortowaniu: "<<endl;
+    
+    sort_wstaw(lista, ile);
+    drukuj(lista, ile);
+    
+    return 0;
+}

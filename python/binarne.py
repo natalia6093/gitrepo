9bafc2dae17769dabd6ad2a4ef73f31e69c12a86
natalia6093 <nzacharska142@gmail.com>
2018-01-08 12:29:04 +0100

    binarne5

--- python/binarne.py
+++ python/binarne.py
@@ -11,30 +11,29 @@ def wyszukiwanie_liniowe(l, e):
     return -1
 
 
-def wyszukiwanie_binarne(l, e):
+def wyszukiwanie_bin_iter(l, e):
     lewy, prawy = 0, len(l) - 1
     while lewy < prawy:
         srodek = floor((lewy + prawy) / 2)
-        print(srodek)
         if e <= l[srodek]:
             prawy = srodek
         else:
             lewy = srodek + 1
 
     if l[lewy] == e:
-        return lewy
+        return lewy  # element znaleziono
 
-    return -1
+    return -1  # elementu nie znaleziono
 
 
 def wyszukiwanie_bin_rek(lewy, prawy, lista, el):
 
     if lewy > prawy:
-        return -1
+        return -1  # elementu nie znaleziono
 
     srodek = floor((lewy + prawy) / 2)
     if el == lista[srodek]:
-        return srodek
+        return srodek  # element znaleziono
 
     if el < lista[srodek]:
         return wyszukiwanie_bin_rek(lewy, srodek - 1, lista, el)
@@ -48,7 +47,7 @@ def main(args):
     print (wyszukiwanie_liniowe(lista, el))
     lista.sort()
     print(lista)
-    print (wyszukiwanie_binarne(lista, el))
+    print (wyszukiwanie_bin_iter(lista, el))
     print (wyszukiwanie_bin_rek(0, len(lista) - 1, lista, el))
 
     return 0

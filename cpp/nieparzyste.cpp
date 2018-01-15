ff0bf34ad2597dfef47526e6a99d91954b784668
natalia6093 <nzacharska142@gmail.com>
2018-01-15 12:33:01 +0100

    nieparzyste

---
+++ cpp/nieparzyste.cpp
@@ -0,0 +1,20 @@
+/*
+ * nieparzyste.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int n;
+    cout << "Podaj n:"<< endl;
+    cin >> n;
+    for (int i = 1; i < n; i += 2 ){
+        cout << i << " ";
+    }
+	return 0;
+}
+

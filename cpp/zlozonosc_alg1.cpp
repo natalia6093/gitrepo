fcef0d343c138710c4869f3e251cc34066e7b529
natalia6093 <nzacharska142@gmail.com>
2018-01-16 10:35:42 +0100

    algorytmy

---
+++ cpp/zlozonosc_alg1.cpp
@@ -0,0 +1,20 @@
+/*
+ * zlozonosc_alg1.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int a = 0;
+    cout << "Podaj a:"<< endl;
+    cin >> a;
+    for (a = 1; a < 100, a+=2){
+        
+        }
+	return 0;
+}
+

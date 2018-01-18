b2b5634daedbd8c56deb2816bb5ce1205c7b4f57
natalia6093 <nzacharska142@gmail.com>
2018-01-18 08:45:29 +0100

    algorytmy

--- cpp/Zacharska_alg1.cpp
+++ cpp/Zacharska_alg1.cpp
@@ -9,12 +9,13 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	int a = 0;
+	int a;
     cout << "Podaj a:"<< endl;
     cin >> a;
-    for (a = 1; a < 100, a+=2){
-        
+    for ( a = 0; a < 100; a+=2){
+        cout <<a<< endl;
         }
+   
 	return 0;
 }
 

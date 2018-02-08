579095e8513948da6d1fbdfab514a4bea24a73dc
natalia6093 <nzacharska142@gmail.com>
2018-02-08 08:04:19 +0100

    szyfrowanie

--- python/szyfra_cezara.py
+++ python/szyfra_cezara.py
@@ -1,27 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-def szyfruj_cezar(tekst, klucz):
-
-    klucz = klucz % 26
+def szyfruj_cezara(tekst, klucz):
+    """Szyfrowanie tekstu za pomocą szyfru Cezara"""
     szyfrogram = ""
+    klucz = klucz % 26
     for znak in tekst:
-        znak = znak.upper()  # lower zmniejsza liczbę
-        ascii = ord(znak) + klucz
-        if ascii > 90:
-            ascii -= 26
+        if ord(znak) > 64 and ord(znak) < 91:
+            ascii = ord(znak) + klucz
+            if ascii > 90:
+                ascii -= 26
+        elif ord(znak) > 96 and ord(znak) < 123:
+            ascii = ord(znak) + klucz
+            if ascii > 122:
+                ascii -= 26
+        elif ord(znak) == 32:
+            ascii = 32
         szyfrogram += chr(ascii)
     return szyfrogram
 
 
-def main(args):
+def deszyfruj(szyfrogram, klucz):
+    tekst = ""
+    klucz = klucz % 26
+    for znak in szyfrogram:
+        if ord(znak) > 64 and ord(znak) < 91:
+            ascii = ord(znak) - klucz
+            if ascii > 90:
+                ascii -= 26
+        elif ord(znak) > 96 and ord(znak) < 123:
+            ascii = ord(znak) - klucz
+            if ascii > 122:
+                ascii -= 26
+        elif ord(znak) == 32:
+            ascii = 32
+        tekst += chr(ascii)
+    return tekst
 
+
+def main(args):
     tekst = input("Podaj tekst: ")
     klucz = int(input("Klucz: "))
+    szyfrogram = szyfruj_cezara(tekst, klucz)
 
-    szyfrogram = szyfruj_cezar(tekst, klucz)
     print(szyfrogram)
+    print(deszyfruj(szyfrogram, klucz))
     return 0
 
 

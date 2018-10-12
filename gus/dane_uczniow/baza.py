2722ea194b61d68b95b36d50cdc60b88c34edaea
natalia6093 <koszyk63>
2018-10-12 13:29:39 +0200

    sql

---
+++ gus/dane_uczniow/baza.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  baza.py
+import csv
+
+
+def czytaj_dane(plik, separator=","):
+    with open (plik, newline='', encoding='utf-8') as plikcsv:
+        tresc = csv.reader(plikcsv, delimiter=separator)
+    print(tresc)
+
+
+def main(args):
+    czytaj_dane('nazwiska.txt', ' ')
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
---
+++ gus/dane_uczniow/szkola.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS nazwiska;
+CREATE TABLE nazwiska (
+    nr_ucznia INTEGER PRIMARY KEY NOT NULL, 
+    nazwisko TEXT (20) NOT NULL,
+    imie1 TEXT (20),
+    imie2 TEXT (20)
+);
+
+DROP TABLE IF EXISTS dane_osobowe;
+CREATE TABLE dane_osobowe (
+    nr_ucznia INTEGER,
+    dzien INTEGER,
+    miesiac INTEGER,
+    rok INTEGER, 
+    miejsce_urodz TEXT (20) DEFAULT "Gdańsk",
+    wojewodztwo TEXT (25) DEFAULT "pomorskie" ,
+    FOREIGN KEY (nr_ucznia) REFERENCES nazwiska(nr_ucznia)
+);
+
+DROP TABLE IF EXISTS oceny;
+CREATE TABLE oceny (
+    nr_ucznia INTEGER,
+    zach TEXT,
+    rel DECIMAL DEFAULT NULL,
+    pol DECIMAL,
+    ang DECIMAL,
+    niem DECIMAL,
+    mat DECIMAL,
+    his DECIMAL,
+    geo DECIMAL,
+    bio DECIMAL,
+    fiz DECIMAL,
+    tech DECIMAL,
+    inf DECIMAL DEFAULT NULL,
+    pla DECIMAL,
+    po DECIMAL ,
+    wf DECIMAL DEFAULT NULL,
+    FOREIGN KEY (nr_ucznia) REFERENCES nazwiska(nr_ucznia)
+);
+
+INSERT INTO nazwiska(nr_ucznia, nazwisko, imie1, imie2)
+VALUES (9201, "Adamczuk", "Agata", "");
+INSERT INTO nazwiska
+VALUES (9802, "Adamiuk", "Marta", "");

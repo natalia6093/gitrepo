35bcdb810e184acf06421ece60a4fac388517979
natalia6093 <koszyk63>
2018-10-19 12:25:45 +0200

    sql

--- gus/dane_uczniow/baza.py
+++ gus/dane_uczniow/baza.py
@@ -4,21 +4,45 @@
 #  baza.py
 import csv
 import sqlite3
+import os.path
+
+def czy_jest(plik):
+    if not os.path.isfile(plik):
+        print("Plik {} nie istnieje". format(plik))
+        return False
+    return True
 
 def czytaj_dane(plik, separator=","):
     dane = []  # pusta lista
+    
+    if not czy_jest(plik):
+        return dane
+    
     with open (plik, newline ='', encoding='utf-8') as plikcsv:
         tresc = csv.reader(plikcsv, delimiter=separator)
         for rekord in tresc:
             dane.append(rekord)
-    print (dane)
+    return dane
     
 
 def main(args):
-    con = sqlite3.connect('baza.db')
+    ###KONFIGURACJA###
+    baza_nazwa = 'szkola'
+    tabele = ['nazwiska', 'dane_osobowe', 'oceny']
+    roz = '.txt'
+    ##################
+    
+    
+    
+    con = sqlite3.connect(baza_nazwa + 'baza.db')
     cur = con.cursor() # obietk tzw. kursora
-    czytaj_dane('nazwiska.txt', ' ' )
-    #with open('szkola.sql', 'r') as plik:
+    
+    if not czy_jest(baza_nazwa + '.sql'):
+        return
+    
+    with open(baza_nazwa + '.sql') as plik:
+        cur.executescript(plik.read())
+        
     #   pass
     
     #czytaj_dane('nazwiska.txt', ' ')

6e379391cea2bbcbcd662c2941e3e366c3019098
natalia6093 <koszyk63>
2018-10-29 09:41:22 +0100

    sql

--- gus/uczniowie/kwerendy.py
+++ gus/uczniowie/kwerendy.py
@@ -6,13 +6,22 @@ import sqlite3
 
 def kwerenda1(cur):
     cur.execute("""
-        SELECT  klasa, COUNT(nazwisko) AS ilu FROM klasy
-        INNER JOIN uczniowie ON klasy.id=uczniowie.id_klasa
-        GROUP BY klasa
-        ORDER BY ilu DESC
+        WITH srednie AS(
+            SELECT nazwisko, AVG(ocena) AS srednia, klasa FROM uczniowie
+            INNER JOIN oceny ON uczniowie.id=oceny.id_uczen
+            INNER JOIN klasy ON uczniowie.id_klasa=klasy.id
+            GROUP BY uczniowie.id
+        ) SELECT AVG(srednia), klasa FROM srednie
+          GROUP BY klasa
     """)
-        #ORDER BY klasa ASC
-        # ~SELECT nazwisko, imie1, dzien, miesiac, rok FROM nazwiska
+        # limit 10 - najlepsi z 10 wyników  DESC malejące ASC rosnące 
+        # ~SELECT imie, nazwisko, COUNT(ocena) AS ilu FROM oceny
+        # ~INNER JOIN uczniowie ON oceny.id=uczniowie.id_klasa
+        # ~GROUP BY ocena
+        # ~ORDER BY ilu DESC
+    #        ORDER BY klasa ASC - sortowanie rekordów
+
+     # ~SELECT nazwisko, imie1, dzien, miesiac, rok FROM nazwiska
         # ~INNER JOIN dane_osobowe 
         # ~ON nazwiska.nr_ucznia=dane_osobowe.nr_ucznia
         # ~WHERE nazwiska.nr_ucznia=9201
@@ -27,7 +36,7 @@ def main(args):
     baza_nazwa = 'uczniowie'
     tabele = ['uczniowie', 'klasy', 'przedmioty', 'oceny']
     roz = '.csv'
-    naglowki = False
+    naglowki = True
     ####################
     
     con = sqlite3.connect(baza_nazwa + '.db')

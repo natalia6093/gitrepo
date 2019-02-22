090e48821d31f837068f42c75bac8b324e784b44
natalia6093 <koszyk63>
2019-02-22 13:15:18 +0100

    uczniowie

--- cpp/pliki.cpp
+++ cpp/pliki.cpp
@@ -11,14 +11,14 @@
 
 using namespace std;
 
-void zmienNazwe1(char t1[], char t2[]) {
-    // kopiowanie znaków z tablicy do tablicy
+void zmienNazwe1(char t1[], char t2[]){
+    // kopiowanie znaków z tablicy do tablicy 
     // METODA 1
-    int i=0;
+    int i = 0;
     while(t1[i] != '.') {
         t2[i] = t1[i];
         i++;
-    }
+    } 
     t2[i]='.';
     t2[++i]='b';
     t2[++i]='a';
@@ -26,28 +26,26 @@ void zmienNazwe1(char t1[], char t2[]) {
     t2[++i]='\0';
 }
 
-
-void zmienNazwe2(char t1[], char t2[]) {
-    // kopiowanie znaków z tablicy do tablicy
+void zmienNazwe2(char t1[], char t2[]){
+    // kopiowanie znaków z tablicy do tablicy 
     // METODA 2
-    // tekst.txt
     char *wsk;
     strcpy(t2, t1);
-    wsk = strstr(t2,".txt");
+    wsk = strstr(t2, ".txt");
     strncpy(wsk, ".bak", 4);
 }
 
 int liczZnaki(char nazwa[]) {
     char kopia[15];
     zmienNazwe2(nazwa, kopia);
-    cout << kopia;
+    cout << kopia << endl;
     
     //otwieranie pliku
     ifstream wejscie(nazwa);
-    if (!wejscie) { cout << "Brak pliku!"; return 1; }
+    if (!wejscie) {cout << "Brak pliku!"; return 1;}
     
-    ofstream wyjscie(kopia)
-    if (!wyjscie) { cout << "Błąd pliku!"; return 1; }
+    ofstream wyjscie(kopia);
+    if (!wyjscie) {cout << "Brak pliku!"; return 1;}
     
     char znak;
     int ile, ileal, ilenum, ilealnum;
@@ -56,19 +54,23 @@ int liczZnaki(char nazwa[]) {
     while(!wejscie.eof()) {
         wejscie.get(znak); // odczytanie poj. znaku
         if (wejscie) ile++;
-        if (wejscie && isalpha(znak)) ileal++; //liczymy litery
-        if (wejscie && isdigit(znak)) ilenum++; //liczymy litery
+        if (wejscie && isalpha(znak)) ileal++;
+        if (wejscie && isdigit(znak)) ilenum++; 
         if (wejscie && isalnum(znak)) {
-            ileal++; //liczymy litery
-            wyjscie.put(znak) // zapisanie znaku do pliku
+            ilealnum++;
+            wyjscie.put(znak); // zapisanie znaku do pliku
         }
+        if ((int)znak == 10)
+            wyjscie.put('\n');
     }
     
     wejscie.close(); wyjscie.close();
-    cout << "Znaków: " << ile << endl;
-    cout << "Liter: " << ile << endl;
-    cout << "Cyfr: " << ile << endl;
+    cout << "Znaków:" << ile << endl;
+    cout << "Liter: " << ileal << endl;
+    cout << "Cyfr:" << ilenum << endl;
+    return ile;
 }
+
 
 
 int main(int argc, char **argv)
@@ -78,6 +80,7 @@ int main(int argc, char **argv)
     cin.getline(nazwa, 15);
     cout << nazwa << endl;
     liczZnaki(nazwa);
+    
+        
 	return 0;
 }
-

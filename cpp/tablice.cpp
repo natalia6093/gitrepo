e329eed63bdc48079fb2e01aa968b8d51769522c
natalia6093 <koszyk63>
2019-03-01 12:21:40 +0100

    uczniowie

--- cpp/tablice.cpp
+++ cpp/tablice.cpp
@@ -5,7 +5,19 @@
 #include <iostream>
 using namespace std;
 
-
+void wprowadz(float *tab, int ile) {
+    for (int i=0; i<ile; i++) {
+        cout << "Ocena: ";
+        cin >> *(tab+i);
+    }
+}
+    
+void drukuj(float *tab, int ile) {
+    for (int i=0; i<ile; i++) {
+        cout << *(tab+i) << " ";
+    }
+    cout << endl;
+}
 int tab1W() {
     float *tb = NULL;
     int ile;
@@ -16,9 +28,14 @@ int tab1W() {
         tb = new float[ile];
     } catch(bad_alloc) {
         cout << "Za mało pamięci!";
-        return 1
+        return 1;
     }
+    wprowadz(tb, ile);
+    drukuj(tb, ile);
+    return 0;
 }
+
+   
 
 int main(int argc, char **argv)
 {

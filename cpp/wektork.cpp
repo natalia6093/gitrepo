0d137782622fc1a8388e24c4ad5c0e44486a5252
natalia6093 <koszyk63>
2019-03-08 12:16:00 +0100

    uczniowie

--- cpp/wektork.cpp
+++ cpp/wektork.cpp
@@ -12,15 +12,22 @@ class Wektor {
     public:
         void pobierz(int);
         void wypisz();
-    }
+};
 
 void Wektor::pobierz(int nr) {
     cout << "Podaj współrzędne " << nr << " wektora: " << endl;
-    }
+    cin >> x;
+    cin >> y;
+}
+
+void Wektor::wypisz() {
+    cout << "[" << x << "," << y << "]" << endl;
+}
 
 int main(int argc, char **argv)
 {
-	
+	Wektor w1;
+    w1.pobierz(1);
 	return 0;
 }
 

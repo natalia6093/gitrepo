8cde821182637884e334b2879d7fd1de53effaef
natalia6093 <koszyk63>
2019-03-08 12:08:36 +0100

    uczniowie

---
+++ cpp/wektork.cpp
@@ -0,0 +1,26 @@
+/*
+ * wektork.cpp
+ */
+ 
+#include <iostream>
+using namespace std;
+
+class Wektor {
+    private:
+        double x;
+        double y;
+    public:
+        void pobierz(int);
+        void wypisz();
+    }
+
+void Wektor::pobierz(int nr) {
+    cout << "Podaj współrzędne " << nr << " wektora: " << endl;
+    }
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
